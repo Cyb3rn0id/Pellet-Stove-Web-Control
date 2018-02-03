384261058676841a3636f9a89431c5514739726a
Giovanni (Gianni) Bernardo <settorezero@gmail.com>
2018-02-03 16:36:25 +0100

    Update and rename PelletStoveWebControl.ino to PelletStoveSimpleControl.ino

--- PelletStoveWebControl/PelletStoveSimpleControl.ino
+++ PelletStoveWebControl/PelletStoveSimpleControl.ino
@@ -1,12 +1,11 @@
 /*
-PELLET STOVE WEB CONTROL
+PELLET STOVE SIMPLE CONTROL
 Web Remote control for Pellet Stoves based on some "Micronova" controllers
 Tested on UNGARO Maia Blend 34 model
 Copyright (c) 2017 Giovanni Bernardo (CYB3rn0id)
 http://www.settorezero.com
 http://www.facebook.com/settorezero
 http://www.twitter.com/settorezero
-
 DESCRIPTION
 -----------
 This application runs on an NodeMCU Devkit connected via Wi-Fi in a local network 
@@ -16,14 +15,12 @@ You can read the ambient temperature too and check if the stove is working or no
 Page is pin/password protected.
 Maybe works on every stove that has a Micronova controller board with double led 
 display and an IR remote with 4 buttons
-
 PREREQUISITES
 ------------- 
 (libraries to be installed first : Menù Sketch -> #include library -> Library management)
 - OneWire (by Paul Stoffregen) => https://www.pjrc.com/teensy/td_libs_OneWire.html 
 - DallasTemperature (by Miles Burton) => https://github.com/milesburton/Arduino-Temperature-Control-Library 
 - IRremoteESP8266 (by Mark Szabo) => https://github.com/markszabo/IRremoteESP8266 
-
 SETTINGS for Arduino IDE for use with NodeMCU Devkit
 ----------------------------------------------------
 Board:              Generic ESP8266 Module
@@ -33,14 +30,12 @@ CPU frequency:      80MHz
 Flash size:         4M (3M SPIFFS)
 Reset method:       nodemcu
 Upload speed:       115200
-
 NOTE on NodeMCU Analog input
 ----------------------------
 NodeMCU Devkit has a voltage divider on Analog input of ESP8266.
 On a bare ESP8266 module you cannot give more than 1V on analog input
 On NodeMCU devkit, since the voltage divider,
 you can give up to 3.3V (3.3V = reading of 1024).
-
 NOTE on NodeMCU Powering
 ------------------------
 The NodeMCU devkit has an on-board 3.3V voltage regulator.
@@ -49,21 +44,17 @@ you can obtain a 5V voltage on "VU" pin (in this project 5V is used for powering
 the Active Buzzer).
 OR you can power NodeMCU giving 5V on VIN pin (in this case you'll obtain the 5V
 from this pin).
-
 MIT License
 -----------
 Copyright (c) 2017 Giovanni Bernardo
-
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -96,14 +87,15 @@ SOFTWARE.
 #define D10  1      // TX
 */
 
-// BUZZER
-// An *active* Buzzer is connected on D0 (GPIO16) driven by BC337 (+ of buzzer on 5V!)
-// 5V on NodeMCU can be found on VU pin if you're powering it from USB
-// or on VIN pin if you're giving 5V on this pin instead of USB
-#define BUZZER 16
+// Used IOs
+#define BUZZER        16  // active buzzer. Note: while uploading, GPIO16 goes high!
+#define BUTTON        12  // used for reset settings
+#define WIFILED       2   // wifi connected led
+#define RECV_PIN      14  // used only for sketch IRReceive
+#define IRLED         4   // infrared led
+#define ONE_WIRE_BUS  5   // DS1820 on D1 (GPIO5) - don't forget a 4K7 pull-up (to 3.3V!)
 
-#define ONE_WIRE_BUS 5 // DS1820 on D1 (GPIO5) - don't forget a 4K7 pull-up (to 3.3V!)
-IRsend irsend(4); // IR Led on D2 (GPIO4) - direct driven without resistor
+IRsend irsend(IRLED); // IR Led on D2 (GPIO4) - direct driven without resistor
 OneWire oneWire(ONE_WIRE_BUS); // Setup a oneWire instance
 DallasTemperature Temperature(&oneWire); // Pass our oneWire reference to Dallas Temperature. 
 
@@ -124,9 +116,6 @@ String pinPs="1234"; // your *numeric* pin/password for accessing the page
 
 // Milliseconds for "stay alive" cochlea running
 #define msCo  12000L
-
-// Page title
-String pageTitle="Stove";
 
 // data used for static IP configuration
 #ifdef USE_STATIC_IP
@@ -157,7 +146,7 @@ uint16_t T_UP[23] = {6710, 2352,  3422, 1562,  894, 732,  918, 1572,  1746, 758,
 uint16_t T_DN[19] = {6726, 2390,  3394, 1536,  944, 734,  916, 1576,  3394, 1538,  942, 736,  918, 1534,  4246, 1536,  190, 86,  1602};  // UNKNOWN 56631A11
 
 // Return Temperature value from DS1820 sensor in string format
-String getTemp(void)
+String getTemperature(void)
  {
  float T=0;
  char stringTemp[7];
@@ -222,76 +211,68 @@ String clientIP(void)
   return server.client().remoteIP().toString();  
   }
 
+// turn on/off the wifi led
+void turnLed(bool v)
+  {
+  v?digitalWrite(WIFILED,LOW):digitalWrite(WIFILED,HIGH);  
+  }
+
+
 // Return HTML page
 // every button is a "Submit"-type button
 String Index_Html(void)
-	{
- 	return
-	"<!DOCTYPE HTML>"
-	"<html>"
-	"<head>"
-	"<title>"+pageTitle+"</title>"
-	"<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0\">"
-	"<style type=\"text/css\">"
-	"body{font-family:arial; font-size:15pt; font-weight:bold;}\n"
-	".bu{font-family:arial; font-weight:bold; font-size:27pt; color:#ffffff; text-align:center; padding:7px; margin:3px; border:0; border-radius:15px; box-shadow:0 8px #666666; outline:none;}\n"
-	".bu:active{box-shadow:0 3px #333333; transform:translateY(4px);}\n"
-	".tx{font-family:arial; font-size:26pt; text-align:left; padding:8px; margin:4px; border:2px #c9c9c9 solid; border-radius:15px;}\n"
-        ".sm {color:#585858; text-decoration:none; font-family:tahoma,arial; font-size:12pt; font-weight:normal; font-variant:small-caps; text-align:center; padding:8px; margin-top:10px; display:block;}\n"
-	"</style>"
-        "<script language=\"javascript\">\n"
-        "xmlhttp=null;\n"
-        "var sensorValues=[];\n"
-        "function getValues()\n"
-        "{"
-        "setTimeout('getValues()', 2000);\n"
-        "if (window.XMLHttpRequest)\n"
-        "{xmlhttp=new XMLHttpRequest();}\n"
-        "else\n"
-        "{xmlhttp=new ActiveXObject('Microsoft.XMLHTTP');}\n"
-        "xmlhttp.open('GET','/getValues',false);\n"
-        "xmlhttp.send(null);\n"
-        "if (xmlhttp.responseText!=\"\")\n"
-        "{sensorValues = xmlhttp.responseText.split(\",\");\n"
-        "document.getElementById(\"te\").innerHTML=sensorValues[0];\n"
-        "document.getElementById(\"status\").innerHTML=sensorValues[1];\n"
-        "}\n"
-        "}</script>"
-	"</head>"
-	"<body onLoad=\"getValues()\">"
-	"<div style=\"text-align:center\">"
-	"<div>CONTROLLO REMOTO STUFA</div>"
-        "<div class=\"sm\" id=\"status\" style=\"font-weight:bold\">"+getStoveWorking()+"</div>"
-	"<div class=\"bu\" id=\"te\" style=\"background-color:#996633; width:93%\">"+getTemp()+"</div><br/>"
-        "<div><form action=\"/\" method=\"post\">"
-	"<input type=\"submit\" name=\"submit\" value=\"ON/OFF\" class=\"bu\" style=\"background-color:#ff6600; width:98%\"><br/><br/>"
-	"<input type=\"submit\" name=\"submit\" value=\"Pow +\" class=\"bu\" style=\"background-color:#00FF00; width:47%;\">"
-	"<input type=\"submit\" name=\"submit\" value=\"Temp +\" class=\"bu\" style=\"background-color:#0099FF; width:47%; float:right;\"><br/><br/>"
-	"<input type=\"submit\" name=\"submit\" value=\"Pow -\" class=\"bu\" style=\"background-color:#00CC00; width:47%;\">"
-	"<input type=\"submit\" name=\"submit\" value=\"Temp -\" class=\"bu\" style=\"background-color:#0099AA; width:47%; float:right;\"><br/>"
-	"<span class=\"sm\">Password:</span>"
-        "<input type=\"hidden\" name=\"username\" value=\""+clientIP()+"\">"
-        "<input type=\"number\" name=\"password\" class=\"tx\" style=\"-webkit-text-security:disc; width:93%;\" pattern=\"[0-9]*\" inputmode=\"numeric\">"
-	"</form></div>"
-	"</div>"
-        "<div class=\"sm\">&copy;2017 Giovanni Bernardo</div>"
- 	"</body>"
-	"</html>";
-	
-	// greater webpages can be sent using following method:
-	// taken from https://github.com/esp8266/Arduino/issues/3205
-	// server.setContentLength(HTML_PART_1().length() + HTML_PART_2().length());   (or hardcode the length)
-	// server.send(200,"text/html",HTML_PART_1()); <---Initial send includes the header
-	// server.sendContent((HTML_PART_2()); <--- Any subsequent sends use this function 
-	}
+  {
+  return
+  "<!DOCTYPE HTML>"
+  "<html>"
+  "<head>"
+  "<title>Stufa</title>"
+  "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0\">"
+  "<style type=\"text/css\">"
+  "body{font-family:arial; font-size:15pt; font-weight:bold;}\n"
+  ".bu{font-family:arial; font-weight:bold; font-size:27pt; color:#ffffff; text-align:center; padding:7px; margin:3px; border:0; border-radius:15px; box-shadow:0 8px #666666; outline:none;}\n"
+  ".bu:active{box-shadow:0 3px #333333; transform:translateY(4px);}\n"
+  ".tx{font-family:arial; font-size:26pt; text-align:left; padding:8px; margin:4px; border:2px #c9c9c9 solid; border-radius:15px;}\n"
+  ".sm {color:#585858; text-decoration:none; font-family:tahoma,arial; font-size:12pt; font-weight:normal; font-variant:small-caps; text-align:center; padding:8px; margin-top:10px; display:block;}\n"
+  "</style>"
+  "<script language=\"javascript\">function getValues(){var sensorValues=[];var xhr=new XMLHttpRequest();xhr.open('GET','/values.txt',true);xhr.onload=function(){if (xhr.readyState==4 && xhr.status==200){sensorValues=xhr.responseText.split(\",\");document.getElementById(\"temp\").innerHTML=sensorValues[0];document.getElementById(\"stove\").innerHTML=sensorValues[1];}};xhr.send(null);}setInterval('getValues()', 2000);</script>"
+  "</head>"
+  "<body onLoad=\"getValues()\">"
+  "<div style=\"text-align:center\">"
+  "<div>CONTROLLO REMOTO STUFA</div>"
+  "<div class=\"sm\" id=\"stove\" style=\"font-weight:bold\">"+getStoveWorking()+"</div>"
+  "<div class=\"bu\" id=\"temp\" style=\"background-color:#996633; width:93%\">"+getTemperature()+"</div><br/>"
+  "<div><form action=\"/\" method=\"post\">"
+  "<input type=\"submit\" name=\"submit\" value=\"ON/OFF\" class=\"bu\" style=\"background-color:#ff6600; width:98%\"><br/><br/>"
+  "<input type=\"submit\" name=\"submit\" value=\"Pow +\" class=\"bu\" style=\"background-color:#00FF00; width:47%;\">"
+  "<input type=\"submit\" name=\"submit\" value=\"Temp +\" class=\"bu\" style=\"background-color:#0099FF; width:47%; float:right;\"><br/><br/>"
+  "<input type=\"submit\" name=\"submit\" value=\"Pow -\" class=\"bu\" style=\"background-color:#00CC00; width:47%;\">"
+  "<input type=\"submit\" name=\"submit\" value=\"Temp -\" class=\"bu\" style=\"background-color:#0099AA; width:47%; float:right;\"><br/>"
+  "<span class=\"sm\">Password:</span>"
+  "<input type=\"hidden\" name=\"username\" value=\""+clientIP()+"\">"
+ "<input type=\"number\" name=\"password\" class=\"tx\" style=\"-webkit-text-security:disc; width:93%;\" pattern=\"[0-9]*\" inputmode=\"numeric\">"
+  "</form></div>"
+  "</div>"
+  "<div class=\"sm\">&copy;2017 Giovanni Bernardo</div>"
+  "</body>"
+  "</html>";
+  
+  // greater webpages can be sent using following method:
+  // taken from https://github.com/esp8266/Arduino/issues/3205
+  // server.setContentLength(HTML_PART_1().length() + HTML_PART_2().length());   (or hardcode the length)
+  // server.send(200,"text/html",HTML_PART_1()); <---Initial send includes the header
+  // server.sendContent((HTML_PART_2()); <--- Any subsequent sends use this function 
+  }
 
 void setup(void)
   {
   pinMode(BUZZER,OUTPUT);
   digitalWrite(BUZZER,LOW);
+  pinMode(WIFILED,OUTPUT);
   
   irsend.begin();
   Temperature.begin();
+  turnLed(false);
   Serial.begin(115200);
     
   // workaround to issue 2186
@@ -320,6 +301,7 @@ void setup(void)
   Serial.println(ssid);
   Serial.print("IP address: ");
   Serial.println(WiFi.localIP());
+  turnLed(true);
 
   // double beep for indicating Wi-Fi connected
   digitalWrite(BUZZER,HIGH);
@@ -332,22 +314,22 @@ void setup(void)
     
   // attach functions on server request
   server.on("/", handleRoot);
-  server.on("/getValues",handleAjaxRefresh);
+  server.on("/values.txt",sendValues);
   server.onNotFound(handleNotFound);
   server.begin();
   LastTimeRunning=millis();
   }
 
-void handleAjaxRefresh()
+void sendValues()
   {
-  server.send(200, "text/html", getTemp()+","+getStoveWorking()+",0");  
+  server.send(200, "text", getTemperature()+","+getStoveWorking()+",0");  
   }
 
 void handleRoot()
   {
   if (server.hasArg("submit"))
     {
-	  Serial.println("Form submitted");
+    Serial.println("Form submitted");
     handleSubmit();
     }
   else 
@@ -373,7 +355,7 @@ void handleSubmit()
   Serial.println(toExec+" command requested");
   if (toExec=="ON/OFF")
     {
-	  digitalWrite(BUZZER,HIGH);
+    digitalWrite(BUZZER,HIGH);
     irsend.sendRaw(TOGGLE, 21, 38);
     digitalWrite(BUZZER,LOW);
     server.send(200, "text/html", Index_Html());
@@ -396,9 +378,9 @@ void handleSubmit()
     Serial.println("Requested Power Down");
     }
   else if (toExec=="Temp +")
-	  { 
+    { 
     digitalWrite(BUZZER,HIGH);
-	  irsend.sendRaw(T_UP, 23, 38);
+    irsend.sendRaw(T_UP, 23, 38);
     digitalWrite(BUZZER,LOW);
     server.send(200, "text/html", Index_Html());
     Serial.println("Requested Temperature Up");
@@ -413,7 +395,7 @@ void handleSubmit()
     }
   else
     {
-	  Serial.println("Not a valid command");
+    Serial.println("Not a valid command");
     returnFail("Bad value");
     }
   }// \HandleSubmit
